38c1cf8dddb4e3b5fa01aa218a17f0c72868debb
Tim Smith <tsmith@chef.io>
2017-07-13 14:57:02 -0700

    Loosen up the cookbook deps
    These would have prevented this from running in an up to date environment, especially one with Chef 13
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Resources for managing Kubernetes'
 long_description 'Deploy a Kubernetes cluster and create, destroy, and update Kubernetes Pods, Services, and Replication Controllers'
 version          '1.0.0'
 
-depends 'build-essential', '~> 2.2.3'
-depends 'selinux', '~> 0.9.0'
-depends 'docker', '~> 2.3'
+depends 'build-essential', '>= 2.2.3'
+depends 'selinux', '>= 0.9.0'
+depends 'docker', '>= 2.3'
 
 supports 'rhel'
 supports 'centos'
